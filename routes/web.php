163894994ec29f8b63cbe6a051752cd8e13f54e3
raza <mehmood.kirmani@gmail.com>
2024-09-16 19:10:12 +0500

    chatroom view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ChatAppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/chat/room',[ChatAppController::class,'chatroom'])->name('chatroom');
